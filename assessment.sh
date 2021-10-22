5ae65e3e1edea2204ea0b2f03fcb3b5912b33c57
Felipe34656 <85200226+Felipe34656@users.noreply.github.com>
2021-10-21 21:00:00 -0300

    Update assessment.sh

--- assessment.sh
+++ assessment.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-> assessment.txt
-date >> assessment.txt
-
-echo "Informacoes da Distro utilizada $(hostname -i):" >> assessment.txt
-cat /etc/*-release >> assessment.txt
-echo "================================" >> assessment.txt
-echo "================================" >> assessment.txt
-echo "Informacoes do Kernel do Servidor $(hostname -i):" >> assessment.txt
-uname -a >> assessment.txt
-echo "================================" >> assessment.txt
-echo "================================" >> assessment.txt
-echo "Lista de Usuarios Presente no Servidor $(hostname -i):" >> assessment.txt
-cat /etc/passwd | cut -d: -f1 >> assessment.txt 
-echo "================================" >> assessment.txt
-echo "================================" >> assessment.txt 
-echo "Pacotes instalados no Servidor $(hostname -i):" >> assessment.txt
-dpkg -l >> assessment.txt
-echo "================================" >> assessment.txt
-echo "================================" >> assessment.txt
+pwd
